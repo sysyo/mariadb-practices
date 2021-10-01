ed26dac63cd2f462f985787ded0330d19f91e422
sysyo <youngiya77@gmail.com>
2021-10-01 17:13:27 +0900

    join 연습 2

--- sql-practies/sql연습/join연습.sql
+++ sql-practies/sql연습/join연습.sql
@@ -35,3 +35,26 @@ where b.to_date = '9999-01-01'; 		-- row 선택 조건
 select a.first_name, b.title
 from employees a join titles b on a.emp_no = b.emp_no
 where b.to_date = '9999-01-01'; 		-- row 선택 조건 
+
+-- outer join
+-- insert into dept values(null, '총무');
+-- insert into dept values(null, '영업');
+-- insert into dept values(null, '개발');
+-- insert into dept values(null, '기획');
+select * from dept;
+
+-- insert into emp values(null, '둘리', 1);
+-- insert into emp values(null, '마이콜', 2);
+-- insert into emp values(null, '또치', 3);
+-- insert into emp values(null, '길동', null);
+select * from emp;
+
+select a.name as '이름', b.name as'부서'
+from emp a join dept b on a.dept_no = b.no;
+
+select a.name as '이름', ifnull(b.name, '없음') as'부서'
+from emp a left join dept b on a.dept_no = b.no;
+
+select a.name as '이름', b.name as'부서'
+from emp a right join dept b on a.dept_no = b.no;
+
